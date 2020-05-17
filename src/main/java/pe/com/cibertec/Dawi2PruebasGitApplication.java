bf7f5686496a219facf8afcf326874801d6bd6c3
gidrogoquispe@gmail.com <neydangabriel>
2020-05-16 19:31:37 -0500

    Mi primer commit

---
+++ src/main/java/pe/com/cibertec/Dawi2PruebasGitApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dawi2PruebasGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dawi2PruebasGitApplication.class, args);
+	}
+
+}
